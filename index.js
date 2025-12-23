961f07d84a75698119ac277b6fcbda433c30b17d
Muthomi Kaburu <kabmuthomi01@gmail.com>
2025-12-23 21:27:10 +0300

    done

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+const userInfo = {
+  username: "user1",
+  password: "password123"
+};
 
+function createLoginTracker(userInfo) {
+  let attemptCount = 0;
 
+  const login = (passwordAttempt) => {
+    attemptCount++;
+    
+    if (passwordAttempt === userInfo.password) {
+      return "Login successful";
+    }
+
+    else if (attemptCount > 3) {
+      return "Account locked due to too many failed login attempts";
+    }
+
+    return `Attempt ${attemptCount}: Login failed`;
+  };
+
+  return login;
+}
 
 module.exports = {
   ...(typeof createLoginTracker !== 'undefined' && { createLoginTracker })
